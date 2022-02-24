528be7b211ce18e437afcb3a87112c50dd643030
josehower <josehowerupn@gmail.com>
2022-02-24 20:08:11 +0100

    Add slashes to comments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // I think this may be a good exercise to learn Git
-line two is missing
-
+// line two is missing
 
 // This is a comment created on GitHub
 
 // Hi, how about adding another comment?
-my comment with whatever I wand
-
+// my comment with whatever I wand
