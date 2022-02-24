9e61abe8f0cd554d88901b666cb0bdd4945d1a7c
josehower <josehowerupn@gmail.com>
2022-02-24 20:19:58 +0100

    Add files for the last part of the lecture

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+import { compareAsc, format } from 'date-fns';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd');
+//=> '2014-02-11'
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
+dates.sort(compareAsc);
+//=> [
+//   Wed Feb 11 1987 00:00:00,
+//   Mon Jul 10 1989 00:00:00,
+//   Sun Jul 02 1995 00:00:00
+// ]
+
 // I think this may be a good exercise to learn Git
 // line two is missing
 
@@ -5,3 +25,4 @@
 
 // Hi, how about adding another comment?
 // my comment with whatever I wand
+console.log('These are the dates:', dates);
